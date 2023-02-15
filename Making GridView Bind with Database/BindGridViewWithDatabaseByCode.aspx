e3f6c5e49832b51000fc131013959b3e0be6c0d8
Mirza Arslan <60809539+Mirza57@users.noreply.github.com>
2023-02-15 05:17:54 +0500

    Add files via upload

---
+++ Making GridView Bind with Database/BindGridViewWithDatabaseByCode.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BindGridViewWithDatabaseByCode.aspx.cs" Inherits="Database_Creation.BindGridViewWithDatabaseByCode" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView RowStyle-BackColor="WhiteSmoke" ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" Width="621px" style="text-align:center; background-color:darksalmon">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Making GridView Bind with Database/BindGridViewWithDatabaseByCode.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Database_Creation
+{
+    public partial class BindGridViewWithDatabaseByCode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindGridView();
+        }
+
+
+        /*
+         *  CODE FOR MAKING GRIDVIEW BIND WITH YOUR DATABASE :
+         */
+        private void BindGridView()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+            conn.Open();
+
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.CommandText = "Select * From [userTable]";
+            sqlCommand.Connection = conn;
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataSet ds = new DataSet();
+            sqlDataAdapter.Fill(ds);
+
+            GridView1.DataSource= ds;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
